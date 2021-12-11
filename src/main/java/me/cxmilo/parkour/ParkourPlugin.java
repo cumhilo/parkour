135c3693b297cab27488928e24f7e9c34ffd1da6
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-12-11 00:36:54 -0500

    classes are instantiated when required

--- src/main/java/me/cxmilo/parkour/ParkourPlugin.java
+++ src/main/java/me/cxmilo/parkour/ParkourPlugin.java
@@ -9,17 +9,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
 
-
 /**
  * Parkour plugin main class
  */
 public class ParkourPlugin
         extends JavaPlugin {
 
-    private final Map<UUID, ParkourGame> parkourGames = new HashMap<>();
-    private final Set<Parkour> parkourSet = new HashSet<>();
-    private final Service commandService = new CommandService(this);
-    private final MessageHandler messageHandler = new MessageHandlerProvider(this).get();
+    private Map<UUID, ParkourGame> parkourGames;
+    private Set<Parkour> parkourSet;
+    private Service commandService;
+    private MessageHandler messageHandler;
 
     public static ParkourPlugin getInstance() {
         return JavaPlugin.getPlugin(ParkourPlugin.class);
@@ -28,6 +27,11 @@ public class ParkourPlugin
     @Override
     public void onEnable() {
         saveDefaultConfig();
+
+        this.parkourGames = new HashMap<>();
+        this.parkourSet = new HashSet<>();
+        this.commandService = new CommandService(this);
+        this.messageHandler = new MessageHandlerProvider(this).get();
 
         commandService.start();
     }
