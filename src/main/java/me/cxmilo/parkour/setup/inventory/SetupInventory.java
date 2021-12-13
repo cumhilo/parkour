f1fc7f3773e6d1d2249e1a11746a7a2891da299f
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-12-12 20:07:22 -0500

    create an ugly class

---
+++ src/main/java/me/cxmilo/parkour/setup/inventory/SetupInventory.java
@@ -0,0 +1,48 @@
+package me.cxmilo.parkour.setup.inventory;
+
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import team.unnamed.gui.core.item.type.ItemBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum SetupInventory {
+    ADD(ItemBuilder
+            .newDyeItemBuilder(Material.INK_SACK.name(), DyeColor.GREEN)
+            .setName("§aCheckpoint adder")
+            .setLore("", "Add a checkpoint to the position you right-click with this item", "")
+            .build()),
+    REMOVE(ItemBuilder
+            .newDyeItemBuilder(Material.INK_SACK.name(), DyeColor.RED)
+            .setName("§aCheckpoint remover")
+            .setLore("", "Removes a checkpoint at the position you right-click with this item", "")
+            .build()),
+    EXIT(ItemBuilder
+            .newBuilder(Material.ARROW)
+            .setName("§aExit")
+            .setLore("", "Exit setup mode", "")
+            .build());
+
+    private static final Map<ItemStack, SetupInventory> SETUP_INVENTORY_MAP = new HashMap<>();
+    private final ItemStack itemStack;
+
+    static {
+        for (SetupInventory setupInventory : values()) {
+            SETUP_INVENTORY_MAP.put(setupInventory.getItemStack(), setupInventory);
+        }
+    }
+
+    SetupInventory(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public static SetupInventory getSetupInventory(ItemStack itemStack) {
+        return SETUP_INVENTORY_MAP.get(itemStack);
+    }
+}
