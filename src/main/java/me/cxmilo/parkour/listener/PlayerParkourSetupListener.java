488fe6935419528865974c040ebc8490f7c0326a
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-12-12 20:13:05 -0500

    the class has been "finished", it will be changed as it is quite ugly, it is only for testing purposes.

--- src/main/java/me/cxmilo/parkour/listener/PlayerParkourSetupListener.java
+++ src/main/java/me/cxmilo/parkour/listener/PlayerParkourSetupListener.java
@@ -1,14 +1,29 @@
 package me.cxmilo.parkour.listener;
 
+import me.cxmilo.parkour.Parkour;
+import me.cxmilo.parkour.ParkourPlugin;
+import me.cxmilo.parkour.game.ParkourCheckpoint;
 import me.cxmilo.parkour.setup.ParkourSetupMode;
+import me.cxmilo.parkour.setup.SetupMode;
+import me.cxmilo.parkour.setup.inventory.SetupInventory;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerParkourSetupListener
         implements Listener {
 
+    private final ParkourPlugin plugin;
+
+    public PlayerParkourSetupListener(ParkourPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    // FIXME: This method has many more responsibilities than it should and looks horrible
+    //  it was just for testing, it should be changed.
     @EventHandler
     public void onPlayerInteractWithSetupItem(PlayerInteractEvent event) {
         Player player = event.getPlayer();
@@ -17,6 +32,61 @@ public class PlayerParkourSetupListener
             return;
         }
 
-        // finish the code
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+
+        Block block = event.getClickedBlock();
+        Parkour parkour = Parkour.findByName(player.getMetadata(ParkourSetupMode.METADATA).get(0).asString());
+
+        if (parkour == null) {
+            return;
+        }
+
+        int nextCheckpoint = parkour.getCheckpoints().size() + 1;
+
+        ParkourCheckpoint checkpoint;
+        SetupInventory setupInventory = SetupInventory.getSetupInventory(player.getItemInHand());
+
+        switch (setupInventory) {
+            case ADD: {
+                checkpoint = new ParkourCheckpoint(parkour, block, nextCheckpoint);
+                checkpoint.setLocation(block.getLocation());
+                plugin.getMessageHandler().sendReplacing(
+                        player,
+                        "parkour.checkpoint.add",
+                        "%num%",
+                        checkpoint.getNum(),
+                        "%parkour%",
+                        checkpoint.getParkour().getDisplayName()
+                );
+                break;
+            }
+            case REMOVE: {
+                checkpoint = ParkourCheckpoint.getByBlock(block);
+
+                if (checkpoint != null) {
+                    plugin.getMessageHandler().sendReplacing(
+                            player,
+                            "parkour.checkpoint.remove",
+                            "%num%",
+                            checkpoint.getNum(),
+                            "%parkour%",
+                            checkpoint.getParkour().getDisplayName()
+                    );
+                    checkpoint.remove();
+                }
+                break;
+            }
+            case EXIT: {
+                SetupMode<Player> playerSetupMode = new ParkourSetupMode(plugin, parkour);
+                playerSetupMode.leave(player);
+                break;
+            }
+            default:
+                return;
+        }
+
+        event.setCancelled(true);
     }
 }
