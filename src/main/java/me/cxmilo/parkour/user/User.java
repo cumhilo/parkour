5fb36e9ead9ea98bca83f0bd8360b8262285f380
vCamilx <72714741+vCamilx@users.noreply.github.com>
2022-01-09 20:08:24 -0500

    add User (player wrapper)

---
+++ src/main/java/me/cxmilo/parkour/user/User.java
@@ -0,0 +1,66 @@
+package me.cxmilo.parkour.user;
+
+import me.cxmilo.parkour.find.Findable;
+import me.cxmilo.parkour.game.impl.CompletedGame;
+import me.cxmilo.parkour.game.impl.ParkourGame;
+import me.cxmilo.parkour.message.Lang;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class User
+        implements Findable<Player> {
+
+    private final Player player;
+    private Lang lang;
+    private ParkourGame activeGame;
+    private List<CompletedGame> completedGames;
+
+    /**
+     * Initialize a new {@link Player player} wrapper to store all active and completed games and their language
+     *
+     * @param id player's unique id
+     */
+    public User(UUID id) {
+        this.player = Bukkit.getPlayer(id);
+        this.lang = Lang.ES;
+        this.activeGame = null;
+        this.completedGames = new ArrayList<>();
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Lang getLang() {
+        return lang;
+    }
+
+    public void setLang(Lang lang) {
+        this.lang = lang;
+    }
+
+    public ParkourGame getActiveGame() {
+        return activeGame;
+    }
+
+    public void setActiveGame(ParkourGame activeGame) {
+        this.activeGame = activeGame;
+    }
+
+    public List<CompletedGame> getCompletedGames() {
+        return completedGames;
+    }
+
+    public void setCompletedGames(List<CompletedGame> completedGames) {
+        this.completedGames = completedGames;
+    }
+
+    @Override
+    public Player getFindKey() {
+        return player;
+    }
+}
