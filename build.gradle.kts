8e69038a4d3b55581e0106b3f1fb512f79cfed47
vCamilx <72714741+vCamilx@users.noreply.github.com>
2022-01-09 20:02:04 -0500

    bump up version to '2.0-SNAPSHOT'

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 val pluginName = "parkour"
 
 group = "me.cxmilo.$pluginName"
-version = "1.0-SNAPSHOT"
+version = "2.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -18,25 +18,22 @@ repositories {
     maven("https://repo.unnamed.team/repository/unnamed-public/")
 }
 
-apply(plugin = "java")
-apply(plugin = "com.github.johnrengelman.shadow")
-
 val targetJavaVersion = 8
 java {
-    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
-    sourceCompatibility = javaVersion
-    targetCompatibility = javaVersion
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
 }
 
 dependencies {
     val spigot = "org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT"
 
     compileOnly(spigot)
+    compileOnly("org.jetbrains:annotations:22.0.0")
+
     implementation("team.unnamed.gui:core:2.3.3-SNAPSHOT")
     implementation("me.yushust.message:core:6.0.17")
     implementation("me.yushust.message:sourcetype-bukkit-yml:6.0.17")
-
-    testImplementation(spigot)
 }
 
 tasks.withType<ShadowJar> {
