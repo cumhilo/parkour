c68f9c914a6b8f1502faa330248e67a1ea64557c
vCamilx <72714741+vCamilx@users.noreply.github.com>
2022-01-09 20:16:31 -0500

    update parkour service

--- src/main/java/me/cxmilo/parkour/service/ParkourService.java
+++ src/main/java/me/cxmilo/parkour/service/ParkourService.java
@@ -6,39 +6,31 @@ import me.cxmilo.parkour.ParkourPlugin;
 import me.cxmilo.parkour.storage.Storage;
 import me.cxmilo.parkour.storage.impl.JsonStorage;
 
-import java.io.File;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.logging.Level;
 
 public class ParkourService
         implements Service {
 
-    private final ParkourPlugin plugin;
     private final Storage<Parkour> storage;
     private final ParkourGameRegistry parkourGameRegistry;
 
     public ParkourService(ParkourPlugin plugin) {
-        this.plugin = plugin;
         this.parkourGameRegistry = plugin.getParkourGameRegistry();
         this.storage = new JsonStorage<>(Parkour.class, plugin);
     }
 
     @Override
     public void start() {
-        Set<Parkour> parkourSet = new HashSet<>();
-
-        for (File file : Objects.requireNonNull(storage.getFileFolder().listFiles(), "File cannot be null")) {
-            parkourSet.add(storage.get(file.getName().replace(".json", "")));
-        }
-
-        parkourGameRegistry.updateParkour(parkourSet);
+        // register all files in folder parkour
+        parkourGameRegistry.updateParkour(storage.values());
+        getLogger().log(Level.INFO, "Successfully loaded '" + storage.values().size() + "' parkours");
     }
 
     @Override
     public void stop() {
-        for (Parkour parkour : plugin.getParkourGameRegistry().getParkourSet()) {
-            storage.save(parkour.getUuid().toString(), parkour);
+        for (Parkour parkour : parkourGameRegistry.getParkourSet()) {
+            storage.save(parkour.getDisplayName(), parkour);
+            getLogger().log(Level.INFO, "'" + parkour.getDisplayName() + "' has been successfully saved as a parkour file");
         }
     }
 }
