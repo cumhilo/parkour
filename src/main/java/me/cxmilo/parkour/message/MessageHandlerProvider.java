77f24f53d0ccb0254eb4228edba26f06c6586c0d
vCamilx <72714741+vCamilx@users.noreply.github.com>
2022-01-10 16:44:09 -0500

    update fallback language and refactor specify to User, remove player & command sender specify

--- src/main/java/me/cxmilo/parkour/message/MessageHandlerProvider.java
+++ src/main/java/me/cxmilo/parkour/message/MessageHandlerProvider.java
@@ -1,42 +1,35 @@
 package me.cxmilo.parkour.message;
 
 import me.cxmilo.parkour.ParkourPlugin;
+import me.cxmilo.parkour.user.User;
 import me.yushust.message.MessageHandler;
 import me.yushust.message.MessageProvider;
 import me.yushust.message.bukkit.BukkitMessageAdapt;
 import me.yushust.message.source.MessageSource;
 import me.yushust.message.source.MessageSourceDecorator;
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 public class MessageHandlerProvider {
 
-    private static final String DEFAULT_LANGUAGE = ParkourPlugin.getInstance().getConfig().getString("lang");
+    private final ParkourPlugin plugin;
 
-    private final Plugin plugin;
-
-    public MessageHandlerProvider(Plugin plugin) {
+    public MessageHandlerProvider(ParkourPlugin plugin) {
         this.plugin = plugin;
     }
 
     private MessageSource getMessageSource() {
         return MessageSourceDecorator
                 .decorate(BukkitMessageAdapt.newYamlSource(plugin, "lang_%lang%.yml"))
-                .addFallbackLanguage(DEFAULT_LANGUAGE)
+                .addFallbackLanguage("en")
                 .get();
     }
 
     public MessageHandler get() {
         return MessageHandler.of(
                 MessageProvider.create(getMessageSource(), configurationHandle -> {
-                    configurationHandle.specify(CommandSender.class)
-                            .setLinguist(sender -> DEFAULT_LANGUAGE)
-                            .setMessageSender((sender, s, message) -> sender.sendMessage(message));
-                    configurationHandle.specify(Player.class)
-                            .setLinguist(player -> DEFAULT_LANGUAGE)
-                            .setMessageSender((player, s, message) -> player.sendMessage(message));
+                    configurationHandle.specify(User.class)
+                            .setLinguist(user -> user.getLang().getAbbreviation())
+                            .setMessageSender((user, mode, message) -> user.getPlayer().sendMessage(message));
                     configurationHandle.addInterceptor(s -> ChatColor.translateAlternateColorCodes('&', s));
                 })
         );
